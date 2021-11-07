4080652ee36579564228790bfb060d9d021530c8
vickybot123 <hungviet.nguyen@hotmail.com>
2021-11-07 16:38:50 +0700

    update source, using new algorithm

--- main.cpp
+++ main.cpp
@@ -4,39 +4,39 @@
 
 using namespace std;
 
-struct Boundary
-{
-    int minX;
-    int minY;
-    int maxX;
-    int maxY;
-};
-
-Boundary findBoundary(int x0, int y0, int xn, int yn)
-{
-    Boundary result;
-    result.minX = min(x0,xn);
-    result.minY = min(y0,yn);
-    result.maxX = max(x0,xn);
-    result.maxY = max(y0,yn);
-    return result;
-}
-
-int calcUselessDistance(Boundary boundary, int x, int y)
-{
-    int xUseless = 0;
-    if (x < boundary.minX)
-        xUseless = boundary.minX - x;
-    else if (x > boundary.maxX)
-        xUseless = x - boundary.maxX;
-
-    int yUseless = 0;
-    if (y < boundary.minY)
-        yUseless = boundary.minY - y;
-    else if (y > boundary.maxY)
-        yUseless = y - boundary.maxY;
-    return xUseless + yUseless;
-}
+//struct Boundary
+//{
+//    int minX;
+//    int minY;
+//    int maxX;
+//    int maxY;
+//};
+//
+//Boundary findBoundary(int x0, int y0, int xn, int yn)
+//{
+//    Boundary result;
+//    result.minX = min(x0,xn);
+//    result.minY = min(y0,yn);
+//    result.maxX = max(x0,xn);
+//    result.maxY = max(y0,yn);
+//    return result;
+//}
+//
+//int calcUselessDistance(Boundary boundary, int x, int y)
+//{
+//    int xUseless = 0;
+//    if (x < boundary.minX)
+//        xUseless = boundary.minX - x;
+//    else if (x > boundary.maxX)
+//        xUseless = x - boundary.maxX;
+//
+//    int yUseless = 0;
+//    if (y < boundary.minY)
+//        yUseless = boundary.minY - y;
+//    else if (y > boundary.maxY)
+//        yUseless = y - boundary.maxY;
+//    return xUseless + yUseless;
+//}
 
 int calcTotalManhattanDistance(int x[], int y[], int len)
 {
@@ -68,33 +68,56 @@ int main()
         return 0;
     }
 
-    Boundary boundary = findBoundary(x[0], y[0], x[len-1],y[len-1]);
-    int maxUselessDistance = 0;
-    int uselessPointIndex = -1;
-    for (int i=1; i < len - 1; i++)
-    {
-        int uld = calcUselessDistance(boundary, x[i], y[i]);
-        if(uld <= maxUselessDistance)
-            continue;
+    int lenDecreasedBy1 = len-1;
+    int resultX[lenDecreasedBy1], resultY[lenDecreasedBy1];
 
-        maxUselessDistance = uld;
-        uselessPointIndex = i;
+    int tobeRemoved = -1;
+    int minDistance = INT_MAX;
+    for (int i=1; i<len - 1; i++)
+    {
+        // remove point at i
+        arrays::removeIntArrayAt(x, len, i, resultX);
+        arrays::removeIntArrayAt(y, len, i, resultY);
+
+        int distance = calcTotalManhattanDistance(resultX, resultY, lenDecreasedBy1);
+        cout << "if remove point at " << i << " -> distance: " << distance << endl;
+
+        if (distance < minDistance)
+        {
+            minDistance = distance;
+            tobeRemoved = i;
+        }
     }
 
+    cout << "so, we will remove point at " << tobeRemoved << " to get min remaining distance " << minDistance << endl;
 
-    if (uselessPointIndex == -1)
-    {
-        cout << "there're no useless point, default skip second point";
-        uselessPointIndex = 1;
-    }
-
-    int optimizedX[len-1];
-    int optimizedY[len-1];
-
-    arrays::removeIntArrayAt(x, len, uselessPointIndex, optimizedX);
-    arrays::removeIntArrayAt(y, len, uselessPointIndex, optimizedY);
-
-    int sum = calcTotalManhattanDistance(optimizedX, optimizedY, len-1);
-    cout << sum << endl;
+//    Boundary boundary = findBoundary(x[0], y[0], x[len-1],y[len-1]);
+//    int maxUselessDistance = 0;
+//    int uselessPointIndex = -1;
+//    for (int i=1; i < len - 1; i++)
+//    {
+//        int uld = calcUselessDistance(boundary, x[i], y[i]);
+//        if(uld <= maxUselessDistance)
+//            continue;
+//
+//        maxUselessDistance = uld;
+//        uselessPointIndex = i;
+//    }
+//
+//
+//    if (uselessPointIndex == -1)
+//    {
+//        cout << "there're no useless point, default skip second point";
+//        uselessPointIndex = 1;
+//    }
+//
+//    int optimizedX[len-1];
+//    int optimizedY[len-1];
+//
+//    arrays::removeIntArrayAt(x, len, uselessPointIndex, optimizedX);
+//    arrays::removeIntArrayAt(y, len, uselessPointIndex, optimizedY);
+//
+//    int sum = calcTotalManhattanDistance(optimizedX, optimizedY, len-1);
+//    cout << sum << endl;
     return 0;
 }
